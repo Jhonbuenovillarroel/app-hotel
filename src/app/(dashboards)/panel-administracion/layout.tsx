e2916f155fd9b38b7f87e473627ad1356e45b1db
Jhonbuenovillarroel <jhonadelbuenovillarroel@gmail.com>
2024-04-13 23:39:14 -0500

    Commit 26

--- src/app/(dashboards)/panel-administracion/layout.tsx
+++ src/app/(dashboards)/panel-administracion/layout.tsx
@@ -5,7 +5,6 @@ import SideBar from "./_components/SideBar/sidebar";
 export const metadata: Metadata = {
   title: "Dashboard",
 };
-export const dynamic = "force-dynamic";
 
 export default function RootLayout({
   children,
