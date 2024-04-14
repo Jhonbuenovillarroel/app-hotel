e250d4ea272953a46675d365586cd0aecab3d37e
Jhonbuenovillarroel <jhonadelbuenovillarroel@gmail.com>
2024-04-13 20:03:30 -0500

    Commit 11

--- src/db/hotel-center/getAllHotelCenters.ts
+++ src/db/hotel-center/getAllHotelCenters.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export const getAllHotelCenters = async () => {
   const hotelCenters = await prisma.hotelcenter.findMany({
     include: { rooms: true, images: true },
